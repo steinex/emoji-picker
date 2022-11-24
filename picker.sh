825ab60c15975864a2913c0c660f5673706a3c44
Frank Steinborn <steinex@nognu.de>
2022-11-24 22:35:31 +0100

    change font size

--- picker.sh
+++ picker.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-EMOJI=$(cat ~/.emojis.txt | fuzzel -d -f FiraSans:weight=medium:size=16)
+EMOJI=$(cat ~/.emojis.txt | fuzzel -d -f FiraSans:weight=medium:size=20)
 
-echo $EMOJI | awk '{print $1}'
+EMOJI_ONLY=$(echo "$EMOJI" | awk '{print $1}')
+
+wtype $EMOJI_ONLY
 
