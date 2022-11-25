093d7d615002a6c4470ce91db0d1ada5e2a7414d
Frank Steinborn <steinex@nognu.de>
2022-11-25 01:02:47 +0100

    fix fontsize again

--- picker.sh
+++ picker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-EMOJI=$(cat ~/.emojis.txt | fuzzel -d -f FiraSans:weight=medium:size=20)
+EMOJI=$(cat ~/.emojis.txt | fuzzel -d -f FiraSans:weight=medium:size=16)
 
 EMOJI_ONLY=$(echo "$EMOJI" | awk '{print $1}')
 
